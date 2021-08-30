54f71fdcd7cb47a255fcdec298343284810154a7
yousufrahman <yousufrahman5@gmail.com>
2021-08-30 10:13:50 +0600

    first commit

---
+++ variable-array.js
@@ -0,0 +1,29 @@
+var bottleColor = 'yellow'
+var waterQuantity = 1;
+var isFull = false;
+
+//array 
+var items = ['bottle', 'mug', 'pen' ];
+items.indexOf ('paper'); //-1
+
+// how to add arry 
+items.push('envelope');
+items.push('watch');
+
+// how to out array 
+items.pop();
+
+// conditionals=== how to apply conditionals(if , else if ,else)
+if (items.length >= 4){
+    console.log('You have too many stuff on your desk.')
+}
+// else if tumi joto bar icc to bar dity parby 
+else if (items.length == 4){
+    console.log('You only have one hali items.')
+}
+else if {
+    console.log('WOW! You have a clean desk.')
+}
+else {
+    console.log('WOW! You have a clean desk.')
+}
