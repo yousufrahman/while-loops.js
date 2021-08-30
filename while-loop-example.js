2681acc63b37b5177ecca8fbbc9670d59e32484b
yousufrahman <yousufrahman5@gmail.com>
2021-08-30 16:17:56 +0600

    even jore

---
+++ while-loop-example.js
@@ -0,0 +1,32 @@
+// akasanay 9 projonto asby ,karon akhany 0 thaky shuro horicy ti .jody 1 thaky var dickale kortam ta holet 10 projonto asto 
+
+
+var number = 1; //loop variable
+// while (number < 10){
+//     console.log(number);
+//     number++
+// }
+
+// akhon 10 projonto asby ,karon variable 1 dickule khorobo
+
+// var number = 1; //loop variable
+// while (number < 10){
+//     console.log(number);
+//     number++
+// }
+
+// akhany protak-ta 2 kore bar by ,
+
+
+// odd number
+// while (number <= 20){
+//     console.log(number)
+//     number = number + 2;
+// }
+
+// even numbers 
+number =0;
+while (number <= 20){
+    console.log(number);
+    number = number +2;
+}
