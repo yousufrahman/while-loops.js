8b72111a950c0efb6fb0b0efcf07450bc8daea2a
yousufrahman <yousufrahman5@gmail.com>
2021-08-30 20:57:16 +0600

    length

---
+++ summary.js
@@ -0,0 +1,22 @@
+// basic while loop
+// var i = 1;
+// while (i <= 7) {
+//     console.log(i)
+//     i++
+// }
+// basic for loop
+// for(var i = 1; i <= 7; i++){
+//     console.log(i);
+// }
+
+var items =['bottle', 'mobile','chargen','watch','book', 'pap'];
+
+
+
+
+
+
+for (var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item)
+}
